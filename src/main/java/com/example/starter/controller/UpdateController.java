daf01ff1e57cccb27a64d1eca0dff39a963b440f
Ankita-328 <1974.ankita@gmail.com>
2026-01-23 17:53:15 +0530

    to update user profile

---
+++ src/main/java/com/example/starter/controller/UpdateController.java
@@ -0,0 +1,31 @@
+package com.example.starter.controller;
+
+import com.example.starter.service.UpdateService;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.RoutingContext;
+
+public class UpdateController {
+  private final UpdateService updateService;
+
+  public UpdateController(UpdateService updateService) {
+    this.updateService = updateService;
+  }
+
+  public void updateProfile(RoutingContext ctx) {
+    String userId = ctx.user().principal().getString("userId");
+    JsonObject body = ctx.body().asJsonObject();
+
+    String fullName = body.getString("fullName");
+    String mobileNumber = body.getString("mobileNumber");
+    String courseName = body.getString("courseName");
+    String qualification = body.getString("qualification");
+    Integer experienceYears = body.getInteger("experienceYears");
+    String emailId = body.getString("emailId");
+
+    updateService.updateProfile(userId, fullName, mobileNumber, courseName, qualification, experienceYears,emailId)
+      .subscribe(
+        updatedUser -> ctx.json(updatedUser),
+        error -> ctx.response().setStatusCode(400).end(new JsonObject().put("error", error.getMessage()).encode())
+      );
+  }
+}
